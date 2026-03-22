8fc7b8cac0ec87b5b231b8ea3dcce90824c4be0a
Is-monp <isabellapalencia@uninorte.edu.co>
2026-03-22 09:34:34 -0500

    feat(data): added models, csv parser, data source and repoimp

---
+++ src/lib/features/tap-on-course/data/repositories/tap_course_repository_impl.dart
@@ -0,0 +1,30 @@
+import '../../domain/entities/course_evaluation.dart';
+import '../../domain/entities/group_category.dart';
+import '../../domain/repositories/tap_course_repository.dart';
+import '../datasources/tap_course_datasource.dart';
+import '../parsers/csv_group_parser.dart';
+
+class TapCourseRepositoryImpl implements TapCourseRepository {
+  final TapCourseDatasource datasource;
+  final CsvGroupParser csvParser;
+
+  TapCourseRepositoryImpl({required this.datasource, required this.csvParser});
+
+  @override
+  Future<List<CourseEvaluation>> getCourseEvaluations(String courseId) async {
+    final models = await datasource.getCourseEvaluations(courseId);
+    return models.map((m) => m.toEntity()).toList();
+  }
+
+  @override
+  Future<List<GroupCategory>> getCourseGroups(String courseId) async {
+    final models = await datasource.getCourseGroups(courseId);
+    return models.map((m) => m.toEntity()).toList();
+  }
+
+  @override
+  Future<List<GroupCategory>> importGroupsFromCsv(String csvContent) async {
+    final models = csvParser.parse(csvContent);
+    return models.map((m) => m.toEntity()).toList();
+  }
+}
